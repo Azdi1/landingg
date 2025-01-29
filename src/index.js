c00d1d0427fe9bf94f0604eef0cdf88dc938eb52
Azdi <azdi.kashapov@yandex.ru>
2025-01-30 01:48:40 +0300

    50% порядка в style + webpack

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// import "./block/style.css";
+// import "./vendor/veriables.css";
+// import "./vendor/fonts.css";
+// import "./vendor/normalize.css";
+
+import "./vendor/fonts.css";
+import "./vendor/normalize.css";
+import "./pages/burger.css";
+import "./pages/slider.css";
+import "./block/style.css";
+import "./vendor/veriables.css";
+
+/* page */
+import "./block/content/page.css";
+/* header */
+import "./block/header/header.css";
+import "./block/header/__logo/logo.css";
+import "./block/header/__burger/burger.css";
+import "./block/header/__menu-list/menu-list.css";
+import "./block/header/__button/button.css";
+/* main */
+import "./block/main/__button/button.css";
